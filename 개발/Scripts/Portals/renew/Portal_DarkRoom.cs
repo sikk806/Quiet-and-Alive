ab2efd448649b6c2a3d5f866a7bbaeae51dc5fe1
unknown <sikk806@gmail.com>
2022-11-22 22:27:20 +0900

    hihihi

--- 개발/Scripts/Portals/renew/Portal_DarkRoom.cs
+++ 개발/Scripts/Portals/renew/Portal_DarkRoom.cs
@@ -8,15 +8,17 @@ public class Portal_DarkRoom : MonoBehaviour
     public int PortalNo = 0;
     public Character Character;
     public GameObject Heart;
+    fireFly fireFly;
 
     // DarkRoom, BoneStart, MazeStart, HeartStart
-    float[] x = { 407.7f, 358.0f, 347.0f, 251.5f };
-    float[] y = { -82.2f, -112.0f, -229.0f, -169.0f };
+    float[] x = { 407.7f, 383.14f, 251.5f };
+    float[] y = { -82.2f, -179.68f, -169.0f };
 
     // Start is called before the first frame update
     void Start()
     {
         Character = GameObject.Find("Character").GetComponent<Character>();
+        fireFly = GameObject.Find("fireFly").GetComponent<fireFly>();
     }
 
     // Update is called once per frame
@@ -42,10 +44,26 @@ public class Portal_DarkRoom : MonoBehaviour
     private IEnumerator rePosition()
     {
         Character.anim.SetBool("JumpMap", false);
-        Character.transform.position = new Vector3(x[0], y[0], 1.0f);
-        Character.stop = true;
-        yield return new WaitForSeconds(2.0f);
-        Character.stop = false;
-        
+        if (CompareTag("ThroatToBody"))
+        {
+            PortalNo = 0;
+            Character.stop = true;
+            fireFly.fly = true;
+            Character.transform.position = new Vector2(x[PortalNo], y[PortalNo]);
+            yield return new WaitForSeconds(2.0f);
+            Character.stop = false;
+        }
+        else if (CompareTag("BodyToVisera"))
+        {
+            PortalNo = 1;
+            Character.transform.localScale = new Vector3(0.7f, 0.7f, 1.0f);
+            Character.transform.position = new Vector2(x[PortalNo], y[PortalNo]);
+        }
+        else if (CompareTag("ViseraToHeart"))
+        {
+            PortalNo = 2;
+            Heart.SetActive(false);
+            Character.transform.position = new Vector2(x[PortalNo], y[PortalNo]);
+        }
     }
 }
